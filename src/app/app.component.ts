a6e56cb005e96d22e69505a2a6d2a14805ebab3e
Emircan <emircandv787@gmail.com>
2024-07-24 18:46:02 +0300

    component ts was  modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, ReplaySubject, Subject, subscribeOn } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +8,17 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   
-  title = 'rxjsDersleri';
+  title = 'rxjs';
   
   ngOnInit(): void {
-    const observable =  new Observable<number>(data => {
+    // const observable =  new Observable<number>(data => {
 
-      data.next(3);
-      data.next(5);
-      data.next(6);
-      data.complete();
+    //   data.next(3);
+    //   data.next(5);
+    //   data.next(6);
+    //   data.complete();
 
-    });
+    // });
     
     // observable.subscribe(data => {
     //   console.log(data);
@@ -35,15 +35,79 @@ export class AppComponent implements OnInit {
 
 // observable.subscribe(observer);
 
-observable.subscribe(data=>{
-     console.log(data);
+// observable.subscribe(data=>{
+//      console.log(data);
 
+//   });
+
+// const subject = new Subject();
+
+// subject.subscribe(data=>{
+// console.log(`observerA : ${data}`);
+// });
+
+// subject.subscribe(data=>{
+// console.log(`observerB : ${data}`);
+// });
+
+// subject.next(1);
+// subject.next(2);
+// subject.next(3);
+
+// subject.subscribe(data=>{
+//   console.log(`observerC : ${data}`);
+//   });
+  
+// subject.next(4);
+// subject.next(5);
+// subject.next("ahmet");
+// }
+
+// let data : any = "hilmi";
+// const subject = new BehaviorSubject(data);
+
+// subject.subscribe(data=>{
+// console.log(`observerA : ${data}`);
+// });
+
+// subject.subscribe(data=>{
+// console.log(`observerB : ${data}`);
+// });
+
+// subject.next(1);
+// subject.next(2);
+// subject.next(3);
+
+// subject.subscribe(data=>{
+//   console.log(`observerC : ${data}`);
+//   });
+  
+// subject.next(4);
+// subject.next(5);
+// subject.next("ahmet");
+
+
+const subject = new ReplaySubject(5);
+
+subject.subscribe(data=>{
+console.log(`observerA : ${data}`);
+});
+
+subject.subscribe(data=>{
+console.log(`observerB : ${data}`);
+});
+
+subject.next(1);
+subject.next(2);
+subject.next(3);
+
+subject.subscribe(data=>{
+  console.log(`observerC : ${data}`);
   });
 
 
 
 }
-
 
 
 
